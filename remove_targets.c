2aad2c7776b2104df46ce3e6b3aaa92b757436c4
Burak Kırık <burakkirikk@gmail.com>
2023-10-13 20:29:46 +0300

    remove targets problem

---
+++ remove_targets.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <limits.h>
+
+int isInteger(double val){
+    int truncated = (int) val;
+    return(truncated == val);
+}
+
+int destroyTargets(int* nums, int numsSize, int space){
+    int maxNumbers = INT_MIN;
+    int scIndex;
+    int count;
+    for(int i = 0; i<numsSize; i++){
+        count = 0;
+        for(int j = 0; j<numsSize; j++){
+            int isCPosInt = isInteger( (nums[j]-nums[i]) / space) && ( (nums[j]-nums[i]) / space ) >= 0;
+            if(isCPosInt){
+                count++;
+            }
+        }
+        if(count > maxNumbers){
+            maxNumbers = count;
+            scIndex = nums[i];
+        }
+    }
+    return scIndex;
+}
+
+int main(){
+    int arr[3] = {6,2,5};
+
+    printf("%d",destroyTargets(arr,3,100));
+    return 0;
+}
